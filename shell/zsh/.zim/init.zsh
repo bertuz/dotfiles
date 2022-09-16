82247b28626e1d10cac6bb4c352b68ceb2e8069d
mbertamini <matteo@bertamini.net>
2022-09-16 18:33:23 +0200

    updated zim after zimfw install

--- shell/zsh/.zim/init.zsh
+++ shell/zsh/.zim/init.zsh
@@ -1,4 +1,5 @@
 zimfw() { source /Users/mbertamini/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
 zmodule() { source /Users/mbertamini/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
 typeset -g _zim_fpath=()
+source /Users/mbertamini/.dotfiles/shell/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /Users/mbertamini/.dotfiles/shell/zsh/.zim/modules/oblong/oblong.zsh-theme
