f6391c4682ce52ced1cae5ff923718e6361d2caa
Matteo B <matteo.bertamini@telefonica.com>
2022-10-29 14:45:47 +0200

    added scripts and updated installed packaages

--- shell/zsh/.zim/init.zsh
+++ shell/zsh/.zim/init.zsh
@@ -1,11 +1,4 @@
 zimfw() { source /Users/mbertamini/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
 zmodule() { source /Users/mbertamini/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
-typeset -g _zim_fpath=(/Users/mbertamini/.dotfiles/shell/zsh/.zim/modules/git-info/functions)
-fpath=(${_zim_fpath} ${fpath})
-autoload -Uz -- coalesce git-action git-info
-source /Users/mbertamini/.dotfiles/shell/zsh/.zim/modules/environment/init.zsh
-source /Users/mbertamini/.dotfiles/shell/zsh/.zim/modules/input/init.zsh
-source /Users/mbertamini/.dotfiles/shell/zsh/.zim/modules/completion/init.zsh
-source /Users/mbertamini/.dotfiles/shell/zsh/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /Users/mbertamini/.dotfiles/shell/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
+typeset -g _zim_fpath=()
 source /Users/mbertamini/.dotfiles/shell/zsh/.zim/modules/oblong/oblong.zsh-theme
